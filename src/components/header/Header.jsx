31447713f7a9b34df23670c482455d62670d4371
Pedro Cunha <p.victora7x@gmail.com>
2022-11-23 14:54:18 -0300

    Inicializando o projeto

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import styles from "./Header.module.css"
+import Title from "../title/Title"
+import Subtitle from "../subtitle/Subtitle"
+
+ function Header(){
+  return(
+      <div className={styles.header}>
+        <Title>Jogo da Velha</Title>
+        <Subtitle>Criado por Pedro Victor</Subtitle>
+     </div>
+
+  )
+ }
+ export default Header
