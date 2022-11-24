39f846972d90eaa3c09cb5fbf5afcfa00c383891
Pedro Cunha <p.victora7x@gmail.com>
2022-11-24 12:17:30 -0300

    Criado estados no gameOption

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -1,19 +1,18 @@
+import { useState } from "react"
 import GameOption from "../gameOption/GameOption"
 import style from "./Game.module.css"
 
  function Game(){
+     const [gameState, setGameState] = useState(Array(9).fill(0))
      return(
         <div className={style.game}>
-            <GameOption />
-            <GameOption />
-            <GameOption />
-            <GameOption />
-            <GameOption />
-            <GameOption />
-            <GameOption />
-            <GameOption />
-            <GameOption />
+         {
+          gameState.map((value,pos)=> <GameOption
+           key={`game-option-pos-${pos}`} 
+           status={value}
+           />)
 
+         }   
         </div>
      )
  }
--- src/components/gameOption/GameOption.jsx
+++ src/components/gameOption/GameOption.jsx
@@ -1,9 +1,19 @@
-import style from "./GameOption.module.css"
 
-function GameOption(){
+import style from "./GameOption.module.css"
+import Icons from "../icons/Icons"
+
+
+const gameIcon = ({iconName}) => <Icons iconName={iconName} size="25px"/>
+
+function GameOption({status}){
    return(
     <div className={style.gameOption}>
-   
+      {
+        status === 1 && <Icons iconName="circle"/> 
+      }
+      {
+        status === -1 && <Icons iconName="x" />
+      }
     </div>
    )
 }
