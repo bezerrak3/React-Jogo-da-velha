60e3dc0f1cbd9cd14b04ab6a93a475b4adc6fe16
Pedro Cunha <p.victora7x@gmail.com>
2022-12-01 15:49:53 -0300

    Criado lógica pra verificar campeão

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -1,32 +1,53 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import GameOption from "../gameOption/GameOption"
 import style from "./Game.module.css"
 import Icons from "../icons/Icons"
 
+const verifyWinner = [ // linhas possíveis de vitória || 3 honrizontais, 3 verticais, 2 diagonais
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
  function Game(){
-  const [gameState, setGameState] = useState(Array(9).fill(0))
-  const [currentPlayer, setCurrentPlayer ] = useState(1)
+  const [gameState, setGameState] = useState(Array(9).fill(0)) // criação do tabuleiro
+  const [currentPlayer, setCurrentPlayer ] = useState(1) // criação do jogador
+  const [winner, setWinner] = useState(0) // função para verificar campeão
+
   const handleClick = (pos) =>{
-    if(gameState[pos] === 0){
-      let newGameState = [...gameState]
-      newGameState[pos] = currentPlayer
-      setCurrentPlayer(currentPlayer * -1)
+    if(gameState[pos] === 0 && winner === 0 ){ //verificação: 1- se o espaço estiver preenchido com bola(-1) ou "x"(1) não se pode alterar| 2- Quando a função setWinner for diferente de 0 não se pode alterar
+      let newGameState = [...gameState] // forma utilizada para copiar o gameState
+      newGameState[pos] = currentPlayer 
+      
       setGameState(newGameState)
 
     }
-     
   }
+  
+   const verifyGame = () => {
+     verifyWinner.forEach((line)=>{
+        const values = line.map((pos)=> gameState[pos]) // troca(map) as posições dos arrays, pelos cliques dos jogadores e acumula
+        const sum = values.reduce((sum, value)=> sum+ value) // soma os cliques dos jogadores
+        if(sum === 3 || sum === -3) setWinner(sum/3) // verificação de vencedor
+     })
+
+  }
+   useEffect(() => { // hook para fazer verificação, sem interações do usuário. ex: chamada de api pra ser carregada, verificação de player atual
+     setCurrentPlayer(currentPlayer * -1) // função para alterar o player
+     verifyGame()
+   }, [gameState])
      
      return(
 
       <div className={style.gameContent}>
          <div className={style.game}>
            {
-             gameState.map((value, pos) => <GameOption
-               key={`game-option-pos-${pos}`}
-               status={value}
-               onClick={() => handleClick(pos)}
-             />)
+             gameState.map((value, pos) => <GameOption key={`game-option-pos-${pos}`} status={value} onClick={() => handleClick(pos)}/>)
 
            }
          </div>
